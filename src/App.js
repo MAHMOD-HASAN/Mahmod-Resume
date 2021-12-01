d70422e3a826aac47f6491a8be3c5991cc6e91b2
MAHMOD-HASAN <osthir7788@gmail.com>
2021-12-01 19:38:23 +0600

    route setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import ContactMe from './Pages/Home/ContactMe/ContactMe';
 import Footer from './Pages/Home/Footer/Footer';
 import Home from './Pages/Home/Home/Home';
+import MyProjects from './Pages/Home/MyProjects/MyProjects';
 import Navigation from './Pages/Shared/Header/Navigation';
 
 
@@ -18,6 +20,14 @@ function App() {
 
          <Route path='/home'>
             <Home></Home>
+         </Route>
+
+         <Route path='/project'>
+            <MyProjects></MyProjects>
+         </Route>
+
+         <Route path='/contact'>
+            <ContactMe></ContactMe>
          </Route>
 
        </Switch>
